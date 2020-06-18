82ac324cc4f8cb30aaba2eaba6bca2a25245e245
LuisMeirelles <luis.gustavo.souto.meirelles@gmail.com>
2020-06-18 12:30:15 -0300

    Carousel finished in mobile layout

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    const width = window.innerWidth
+    const identifiers = ['#sidebar-collapse', '#sidebar', '#content']
+
+    if(width <= 768) {
+        identifiers.forEach(i => {
+            $(i).addClass('activated')
+        })
+    }
+
+    $(document).on('click', '#sidebar-collapse', () => {
+        identifiers.forEach(i => {
+            $(i).toggleClass('activated')
+        })
+    })
+})
